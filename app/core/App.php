bbc4ad1df3f797ca05758c86063fca1749422063
ryanuarmd <32842859+ryanuarmd@users.noreply.github.com>
2019-08-24 12:43:05 +0700

    var_dump url

--- app/core/App.php
+++ app/core/App.php
@@ -2,8 +2,19 @@
 
 class App {
 
-    public function __construct(){
-        echo 'OK!';
+    public function __construct()
+    {
+        $url = $this -> parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if(isset($_GET['url']) )
+        {
+            $url = $_GET['url'];
+            return $url;
+        }
     }
 
 }
